fd7169d4fe364d81ce37d30cade0ed61d8c9288b
Diana Lim <lim.diana@outlook.com>
2020-07-06 17:11:45 -0700

    Add project files.

---
+++ Limbrary/Controllers/BooksController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Limbrary.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Limbrary.Controllers
+{
+    public class BooksController : Controller
+    {
+        public ActionResult Random()
+        {
+            var book = new Book() { Name = "The Secret Garden" };
+            return View(book);
+        }
+    }
+}
